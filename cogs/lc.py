a42267be5198a838446c4b793706b3ad979e2dba
August Wasilowski <august.wasilowski@gmail.com>
2023-05-02 19:57:24 -0500

    Implemented !selfreflect, a bot that reads everything in .cogs and allows the user to query the contents.
    Signed-off-by: August Wasilowski <august.wasilowski@gmail.com>

--- cogs/lc.py
+++ cogs/lc.py
@@ -1,22 +1,55 @@
 import logging
 import os
 
+from langchain.chains import RetrievalQA
 from langchain.chains.summarize import load_summarize_chain
+from langchain.chat_models.openai import ChatOpenAI
+from langchain.document_loaders import TextLoader
 from langchain.document_loaders import YoutubeLoader
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
 from nextcord.ext import commands
 from pytube import YouTube
+
+from cogs.status import working, wait_for_orders
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
-
 def setup(bot: commands.Bot):
     bot.add_cog(LangChainCog(bot))
 
+
+async def exe_selfreflect(ctx, arg):
+    llm = ChatOpenAI(model='gpt-3.5-turbo', openai_api_key=os.getenv("OPENAI_API_KEY"))
+    embeddings = OpenAIEmbeddings(disallowed_special=(), openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+    root_dir = './cogs/'
+    docs = []
+
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+
+        # Go through each file
+        for file in filenames:
+            if file.endswith(".py"):  # Check if file has .py extension
+                try:
+                    # Load up the file as a doc and split
+                    loader = TextLoader(os.path.join(dirpath, file), encoding='utf-8')
+                    docs.extend(loader.load_and_split())
+                except Exception as e:
+                    logger.error(f"error loading docs {e}")
+
+    logger.info(f"You have {len(docs)} documents\n")
+    docsearch = FAISS.from_documents(docs, embeddings)
+    # Get our retriever ready
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever())
+
+    output = qa.run(arg)
+    await ctx.send(output)
 
 async def exe_ss(ctx, arg):
     loader = YoutubeLoader.from_youtube_url(arg)
@@ -53,16 +86,17 @@ class LangChainCog(commands.Cog):
         # Summary for SHORT YouTube videos. Must be the video ID, not the whole URL. i.e. "g8LlwlCU0EA" not
         # "https://youtu.be/g8LlwlCU0EA"
         yt = YouTube(arg)
-        # await status.working(ctx)
         await exe_ss(ctx, yt.video_id)
-        # await status.wait_for_orders(ctx)
 
     @commands.command()
     async def ls(self, ctx, *, arg):
         # Summary for SHORT YouTube videos. Must be the video ID, not the whole URL. i.e. "g8LlwlCU0EA" not
         # "https://youtu.be/g8LlwlCU0EA"
         yt = YouTube(arg)
-
-        # await status.working(ctx)
         await exe_ls(ctx, yt.video_id)
-        # await status.wait_for_orders(ctx)
+
+    @commands.command()
+    async def selfreflect(self, ctx, *, arg):
+        await working(self.bot)
+        await exe_selfreflect(ctx, arg)
+        await wait_for_orders(self.bot)
